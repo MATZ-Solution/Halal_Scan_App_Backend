0e6999ae7cafc1afdd66a84101aeed7ce155bc8b
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2025-01-23 19:42:18 +0500

    new commit

--- config/s3.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    
-    bucketName: 'powerhouseassets',
-    
-    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    awsSecretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-1',
-  };
-  
