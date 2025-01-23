0173cba6f496507ccd398afb6a9cf0f2622aa164
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2025-01-23 19:34:13 +0500

    add new commit

--- config/s3.js
+++ config/s3.js
@@ -1,9 +1,9 @@
-module.exports = {
+// module.exports = {
     
-    bucketName: 'powerhouseassets',
+//     bucketName: 'powerhouseassets',
     
-    awsAccessKeyId: "AKIAQPZG37TRU4ABDSXN",
-    awsSecretAccessKey: "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",
-    region: 'us-east-1',
-  };
+//     awsAccessKeyId: "AKIAQPZG37TRU4ABDSXN",
+//     awsSecretAccessKey: "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",
+//     region: 'us-east-1',
+//   };
   
